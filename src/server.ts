72d044163948af20f47499d9523f33a40c2934d2
DFKorn <d1man2192@mail.ru>
2025-11-22 14:43:49 +0300

    set up Stream Chat

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { StreamChat } from "stream-chat";
 
 dotenv.config();
 
@@ -9,6 +10,50 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Initialize Stream Chat client
+const chatClient = StreamChat.getInstance(
+  process.env.STREAM_API_KEY!,
+  process.env.STREAM_API_SECRET!
+);
+
+// Register user with Stream Chat
+app.post(
+  "/register-user",
+  async (req: Request, res: Response): Promise<any> => {
+    const { name, email } = req.body || {};
+
+    if (!name || !email) {
+      return res
+        .status(400)
+        .json({ error: "Name and email are required fields." });
+    }
+
+    try {
+      const userId = email.replace(/[^a-zA-Z0-9_-]/g, "_"); // Using email as user ID
+
+      //Check if user already exists
+      const userResponse = await chatClient.queryUsers({
+        id: { $eq: userId },
+      });
+      //console.log("Existing users:", userResponse);
+      if (userResponse.users.length === 0) {
+        // Add new user to stream chat
+        await chatClient.upsertUser({
+          id: userId,
+          name: name,
+          email: email,
+          role: "user",
+        });
+      }
+      //console.log("Registering user with ID:", userId);
+      res.status(200).json({ userId, name, email });
+    } catch (err) {
+      console.error("Error registering user:", err);
+      return res.status(500).json({ error: "Internal server error." });
+    }
+  }
+);
 
 const PORT = process.env.PORT || 5000;
 
