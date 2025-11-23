7ab45f2cbb775659adced58c65825fdd7797a744
DFKorn <d1man2192@mail.ru>
2025-11-23 16:23:30 +0300

    add Drizzle ORM, Database Config & Neon Adapter

---
+++ src/config/database.ts
@@ -0,0 +1,17 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import { config } from "dotenv";
+
+// Load environment variables from .env file
+config({ path: ".env" });
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in environment variables.");
+}
+
+// Initialize Neon client
+
+const sql = neon(process.env.DATABASE_URL);
+
+// Initialize Drizzle ORM with Neon client
+export const db = drizzle(sql);
